ba010e889ba0ee62e35d30536b79a114a1542919
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-26 13:08:21 -0300

    Check for category before map on RestaurantThumb

--- src/components/RestaurantThumb/index.js
+++ src/components/RestaurantThumb/index.js
@@ -9,30 +9,31 @@ const RestaurantThumb = ({ data }) => {
     Foto_da_Comida: photos,
     Nome_do_Estabelecimento: name,
     Categoria: categories,
-  } = data;
+  } = data
 
-  const photo = (photos && photos.length && photos[0].thumbnails) || null;
+  const photo = (photos && photos.length && photos[0].thumbnails) || null
 
   return (
     <div className={styles.container}>
-      {photo &&
+      {photo && (
         <div className={styles.thumbnail} key={photo.full.url}>
           <Link to={pageNameByNode(data)}>
             <img src={photo.full.url} alt={`Foto de ${name}.`} />
           </Link>
         </div>
-      }
+      )}
 
       <h3 className={styles.title}>{name}</h3>
       <p className={styles.tag}>
-        {
-          categories.map((category, index) => (
-            <span key={category}>{category}{index < categories.length - 1 && ','} </span>
-          ))
-        }
+        {categories?.map((category, index) => (
+          <span key={category}>
+            {category}
+            {index < categories.length - 1 && ","}{" "}
+          </span>
+        ))}
       </p>
     </div>
   )
 }
 
-export default RestaurantThumb;
+export default RestaurantThumb
