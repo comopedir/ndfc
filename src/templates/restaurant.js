07072cf8760f330d88d467cbd2e9ab9e878459bc
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-26 13:17:13 -0300

    Implement pre-commit hook for prettier on staged files

--- src/templates/restaurant.js
+++ src/templates/restaurant.js
@@ -5,26 +5,25 @@ import SEO from "../components/seo"
 import HomeHeader from "../components/HomeHeader"
 import Footer from "../components/Footer"
 
-const RestaurantPage = ({pageContext}) => {
-
-  const { data: internalData } = pageContext;
-  const { data: node } = internalData;
+const RestaurantPage = ({ pageContext }) => {
+  const { data: internalData } = pageContext
+  const { data: node } = internalData
 
   // ↓↓↓ suggestion: deconstruct other data from here ↓↓↓↓
   // the pageContext is feed by /gatsby-node.js createPages method
-  const { 
+  const {
     Nome_do_Estabelecimento: restaurantName,
     Estado: state,
     Cidade: city,
-  } = node;
+  } = node
 
   return (
     <Layout>
       <SEO title="Restaurante" />
       <HomeHeader />
-        <h2>{restaurantName}</h2>
-        <h3>{state}</h3>
-        <h2>{city}</h2>
+      <h2>{restaurantName}</h2>
+      <h3>{state}</h3>
+      <h2>{city}</h2>
       <Footer />
     </Layout>
   )
