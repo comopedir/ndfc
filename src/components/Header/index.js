568e3d99fbf09a75cf1961608b5fb2ad4bc4bd55
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-27 17:56:52 -0300

    Change Header to use Link instead of a in logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import logofood from "../../images/symbol-avocado.png"
 
 import styles from "./styles.module.scss"
@@ -7,13 +8,22 @@ const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles.logo}>
-        <h1><a href="/">não&nbsp;deixe fechar&nbsp;&nbsp;&nbsp; a&nbsp;conta.</a></h1>
+        <h1>
+          <Link href="/">
+            não&nbsp;deixe fechar&nbsp;&nbsp;&nbsp; a&nbsp;conta.
+          </Link>
+        </h1>
         <p className={styles.subtitle}>
           Todas as formas de pedir dos seus lugares favoritos.
         </p>
       </div>
       <div className={styles.menu}>
-        <a className={styles.button} href="https://airtable.com/shrD4fU2g2GD2yzJ5">Cadastrar</a>
+        <a
+          className={styles.button}
+          href="https://airtable.com/shrD4fU2g2GD2yzJ5"
+        >
+          Cadastrar
+        </a>
       </div>
     </header>
   )
