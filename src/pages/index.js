067c90e68cb7c5e5809f1cfe84268e469896967b
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-27 17:54:51 -0300

    Implement color theme logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,11 @@ import SEO from "../components/seo"
 import HomeHeader from "../components/Header"
 import RestaurantList from "../components/RestaurantList"
 import Footer from "../components/Footer"
+import useTheme from "../hooks/useTheme"
 
 const IndexPage = () => {
+  useTheme("home")
+
   return (
     <Layout>
       <SEO title="Home" />
---
+++ src/utils/theming.js
@@ -0,0 +1,14 @@
+import variables from "../assets/styles/variables.scss"
+
+export const themes = {
+  ["Pizza"]: {
+    primary: variables.colorMediumVermilion,
+    secondary: variables.colorKombuGreen,
+    tertiary: variables.colorUnbleachedSilk,
+  },
+  home: {
+    primary: variables.colorKombuGreen,
+    secondary: variables.colorMediumVermilion,
+    tertiary: variables.colorUnbleachedSilk,
+  },
+}
