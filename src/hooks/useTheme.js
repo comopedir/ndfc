15604f8017d187a7bdb47727d2a9aa3e57b553c5
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-27 18:08:16 -0300

    Add library to fix server-side rendering issues

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,24 +1,25 @@
 import { useEffect } from "react"
 import { themes } from "../utils/theming"
+import root from "window-or-global"
 
 const useTheme = category => {
   useEffect(() => {
     const themeColors = themes[category]
-    if (themeColors && document) {
-      document.documentElement.style.setProperty(
+    if (themeColors && root.document) {
+      root.document.documentElement.style.setProperty(
         "--primary-color",
         themeColors.primary
       )
-      document.documentElement.style.setProperty(
+      root.document.documentElement.style.setProperty(
         "--secondary-color",
         themeColors.secondary
       )
-      document.documentElement.style.setProperty(
+      root.document.documentElement.style.setProperty(
         "--tertiary-color",
         themeColors.tertiary
       )
     }
-  }, [category, document])
+  }, [category, root])
 }
 
 export default useTheme
