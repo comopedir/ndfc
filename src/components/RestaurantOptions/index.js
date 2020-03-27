c9ad8e4d29703dfbd81979505b1c6b09b48b30b4
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-26 23:42:40 -0300

    Add link to button accordingly with the button

--- src/components/RestaurantOptions/index.js
+++ src/components/RestaurantOptions/index.js
@@ -10,12 +10,46 @@ const RestaurantOptions = ({
   instagram,
   website,
   services = [],
+  name,
 }) => (
   <div className={styles.container}>
-    {options.map((option, index, array) =>
-      index !== 3 || index !== array.length - 1 || !services.length ? (
+    {options.map((option, index, array) => {
+      let href
+      switch (option) {
+        case "iFood":
+          const searchUrl = "https://www.ifood.com.br/busca?q="
+          href = `${searchUrl}${encodeURI(name)}`
+          break
+        case "Rappi":
+          href = `https://www.rappi.com.br/search?store_type=all&query=${encodeURI(
+            name
+          )}`
+          break
+        case "Whatsapp":
+          href = `whatsapp://send?&phone=${wpp.replace(/\D*/g, "")}`
+          break
+        case "Telefone":
+          href = `tel:${phone.replace(/\D*/g, "")}`
+          break
+        case "DM no Instagram":
+          href = instagram.replace(
+            /(\@)|((\w*\.*)?instagram.com(.br)?\/?)/,
+            "https://www.instagram.com/"
+          )
+          break
+        case "Website":
+          href = website
+          break
+        case "Uber Eats":
+        case "James Delivery":
+        default:
+          break
+      }
+
+      return index !== 3 || index !== array.length - 1 || !services.length ? (
         <OptionButton
-          href="#"
+          key={option}
+          href={href}
           option={option}
           phone={phone}
           wpp={wpp}
@@ -23,12 +57,12 @@ const RestaurantOptions = ({
           website={website}
         />
       ) : (
-        <Button>
+        <Button key="services">
           <p className={styles.label}>Serviços</p>
           <p>{services.join(", ")}</p>
         </Button>
       )
-    )}
+    })}
   </div>
 )
 
