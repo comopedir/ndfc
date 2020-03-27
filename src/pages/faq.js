7225ca4e7cfaf1125ca47901cf36c0b652391208
Renato Bonicio <renatobonicio@gmail.com>
2020-03-26 23:31:40 -0300

    Added Fag page

---
+++ src/pages/faq.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import FAQContent from "../components/FAQContent"
+import Footer from "../components/Footer"
+
+const FaqPage = () => {
+  return (
+    <Layout>
+      <SEO title="FAQ" />
+      <FAQContent />
+      <Footer />
+    </Layout>
+  )
+}
+
+export default FaqPage
