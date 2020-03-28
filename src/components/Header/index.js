bff9c686fbb58ff8139774d0d75e7e3a7f1d1db1
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-27 21:00:32 -0300

    Fix header for mobile breakpoints

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -22,7 +22,18 @@ const Header = () => {
           className={styles.button}
           href="https://airtable.com/shrD4fU2g2GD2yzJ5"
         >
-          Cadastrar
+          <span className={styles.desktopSignup}>Cadastrar</span>
+          <svg
+            className={styles.mobileSignup}
+            width="14"
+            height="14"
+            viewBox="0 0 14 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line y1="7" x2="14" y2="7" stroke-width="2" />
+            <line x1="7" y1="14" x2="7" stroke-width="2" />
+          </svg>
         </a>
       </div>
     </header>
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../Layout"
 import SEO from "../seo"
-import HomeHeader from "../Header"
 import RestaurantList from "../RestaurantList"
 import Footer from "../Footer"
 import useTheme from "../../hooks/useTheme"
