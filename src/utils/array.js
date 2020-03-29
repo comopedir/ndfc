7f487578969f2ff4de463f94758a8f430e651446
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-28 22:11:40 -0300

    Remove accent on sorting

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,7 +1,9 @@
+const removeAccents = require("remove-accents")
+
 export const alphabeticSortArray = (arr = []) =>
   arr.sort((a, b) => {
-    const lowerCaseA = a.toLowerCase()
-    const lowerCaseB = b.toLowerCase()
+    const lowerCaseA = removeAccents(a).toLowerCase()
+    const lowerCaseB = removeAccents(b).toLowerCase()
     if (lowerCaseA < lowerCaseB) return -1
     if (lowerCaseA > lowerCaseB) return 1
     return 0
