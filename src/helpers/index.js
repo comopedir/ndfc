7bd0d52bf5c5e0d09b3a07c022b63e7cba4a2c83
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-03-30 18:33:46 -0300

    Remove whitespace from the page pathname

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,8 +3,10 @@ const removeAccents = require("remove-accents")
 module.exports.pageNameByNode = function(node) {
   const { Instagram: instagram, Nome_do_Estabelecimento: restaurantName } = node
 
+  let path = ""
+
   if (instagram) {
-    return instagram
+    path = instagram
       .toLowerCase()
       .replace("instagram.com/", "")
       .replace("intagram.com/", "")
@@ -14,13 +16,11 @@ module.exports.pageNameByNode = function(node) {
       .replace("http://", "")
       .replace("www.", "")
       .replace("/?hl=pt-br", "")
+  } else if (restaurantName) {
+    path = restaurantName.toLowerCase()
   }
 
-  if (restaurantName) {
-    return restaurantName.toLowerCase().replace(" ", "_")
-  }
-
-  return ""
+  return path.replace(/\s/g, "")
 }
 
 module.exports.pageNameByLocation = function(state, city) {
